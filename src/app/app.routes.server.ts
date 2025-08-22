626288dfa34367592226074088147d3d0a8d000d
Mahd Mohamed <engmahd00@gmail.com>
2025-08-23 02:31:03 +0300

    solve error deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id/:slug',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '1', slug: 'sample-product' },
+      { id: '2', slug: 'another-product' }
+    ]
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '1' },
+      { id: '2' }
+    ]
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
