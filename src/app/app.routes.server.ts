31c3cf115fd3719f0037e64d18b663dd7920e23d
Mahd Mohamed <engmahd00@gmail.com>
2025-09-05 06:54:48 +0300

    solve err

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,28 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'categories/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '6439d5b90049ad0b52b90048' },
+      { id: '6439d5b90049ad0b52b90049' },
+      { id: '6439d5b90049ad0b52b9004a' },
+      { id: '6439d5b90049ad0b52b9004b' },
+      { id: '6439d5b90049ad0b52b9004c' }
+    ]
+  },
+  {
+    path: 'category-details/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '6439d5b90049ad0b52b90048' },
+      { id: '6439d5b90049ad0b52b90049' },
+      { id: '6439d5b90049ad0b52b9004a' },
+      { id: '6439d5b90049ad0b52b9004b' },
+      { id: '6439d5b90049ad0b52b9004c' }
+    ]
+  },
+  {
     path: 'details/:id/:slug',
     renderMode: RenderMode.Prerender,
     getPrerenderParams: async () => [
